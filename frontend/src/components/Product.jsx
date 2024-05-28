138eeb0854f0ba918769ea391a9ad35c1661bc17
Shubham Kadam <shubham.kadam@appgambit.com>
2024-05-28 15:28:57 +0530

    implement react-router

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Product = ({ Product }) => {
   return (
     <Card className="my-3 p-3">
-      <Card.Img src={Product.image} variant="top" />
+      <Link to={`/product/${Product._id}`}>
+        <Card.Img src={Product.image} variant="top" />
+      </Link>
+
       <Card.Body>
         <Card.Title as="div">
           <strong>{Product.name}</strong>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom'
 import './index.css';
 import App from './App';
+import HomeScreen from './screens/HomeScreen';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+    </Route>
+
+  )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
