b32f717abd8af8e95846f4d7e13e1d67d69eba22
Shubham Kadam <shubham.kadam@appgambit.com>
2024-05-31 15:04:10 +0530

    create cart slice with empty reducer and add in store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice";
+import cartSliceReducer from "./slices/cartSlice";
 
 const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
+        cart: cartSliceReducer
     },
     // Adding the api middleware enables caching, invalidation, polling,
     // and other useful features of `rtk-query`.
